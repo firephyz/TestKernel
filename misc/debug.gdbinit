751f860f3b30e138db79bb773fb24d3ee4511d7d
kyle <burgekm@miamioh.edu>
2017-09-10 23:07:46 -0400

    Committing just for keeping some code that might be useful in the future. Specifically the assembly code in _enable_A20. That stuff has nothing to do with A20, its just some test code for getting the hard disk geometry and grabbing a sector. Might use later if I ever want to change the size of the hard disk again.

--- misc/debug.gdbinit
+++ misc/debug.gdbinit
@@ -2,6 +2,8 @@ target remote :1234
 set architecture i386
 symbol-file debug/boot.sym
 hbreak kernel_start
+hbreak boot.s:50
+hbreak boot.s:60
 layout next
 layout next
 layout next
--- run_machine.sh
+++ run_machine.sh
@@ -10,11 +10,18 @@ if [ ! -z "$DEBUG" ]; then
 	konsole -e bash -c 'cd ~/dev/kernel && gdb -iex "source misc/debug.gdbinit"'
 fi
 
-qemu-system-i386 $DEBUG										\
-				 -enable-kvm                          		\
-                 -drive file=./boot.bin,if=ide,format=raw	\
-                 -vga std                             		\
-                 -m 1024M                             		\
-                 -boot d                              		\
-                 -name "Lisp VM"                      		\
-                 -monitor stdio
+# Qemu's disk geometry detection is very strange. Will manually specify for now. Also, the cylinders
+# argument must be (# of cylinders) + 2. Don't know why but Seabios thinks it
+# actually has 2 less cylinders than what you specify here. So for my 129 sector disk right now,
+# chs (2, 0, 3) is the last sector. (63 - 1) + 63 + 3.
+# Option to add if we want seabios to output debug info.
+# -chardev stdio,id=seabios -device isa-debugcon,iobase=0x402,chardev=seabios
+qemu-system-i386 $DEBUG																\
+				 -enable-kvm                          								\
+                 -drive file=./boot.bin,if=ide,format=raw,cyls=5,heads=1,secs=63	\
+                 -vga std                             								\
+                 -m 1024M                             								\
+                 -boot d                              								\
+                 -name "Lisp VM"                      								\
+                 -bios /home/firephyz/src/seabios/out/bios.bin 						\
+                 -chardev stdio,id=seabios -device isa-debugcon,iobase=0x402,chardev=seabios
